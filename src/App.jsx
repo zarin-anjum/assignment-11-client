abc21e0afde808c47bcf7b097a53b3abf760cb63
zarin-anjum <zarinanjum0801@gmail.com>
2026-02-09 00:12:16 +0600

    installed required packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Toaster } from "react-hot-toast";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="p-10">
-      <button className="btn btn-primary">
-        DaisyUI Button 🌼
-      </button>
-    </div>
+    <>
+      <Toaster position="top-center" />
+    </>
   )
 }
 
